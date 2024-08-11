cb4ca27e2ab533770abc25a29b8b6abe5f1f07f5
ZakyTm <zakaria.t149919@gmail.com>
2024-08-11 15:42:38 +0100

    adding the assets folder which has all the necessary png files, and creating a new folder caleld UI which contianes a home page for now for the principle interface of our weather app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:weather_forecast/ui/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Weather_forecast app',
+        home: HomePage());
+  }
+}
