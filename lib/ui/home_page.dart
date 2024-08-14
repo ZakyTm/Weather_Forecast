4dcab27e9de94b5f6719cacc82d87b9032b026d7
ZakyTm <zakaria.t149919@gmail.com>
2024-08-14 05:44:47 +0100

    Extract WeatherItem widget for reusability

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -275,31 +275,58 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.white70,
                     ),
                   ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          height: 60,
-                          width: 60,
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Image.asset('assets/heavyrain.png'),
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        )
-                      ],
-                    ),
-                  ),
+                  WeatherItem(),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class WeatherItem extends StatelessWidget {
+  final int value;
+  final String text;
+  final String unit;
+  final String imageUrl;
+
+  const WeatherItem({
+    super.key,
+    required this.value,
+    required this.text,
+    required this.unit,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Image.asset('imageUrl'),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            value.toString() + unit,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
